d5d27ad25eec2ce88d69afade01f3e9cc40d839d
Kristian Fischer <khfishy@gmail.com>
2021-09-06 11:47:08 -0700

    Add logging debug for pinger selected based on system platform

--- pingers/factory.py
+++ pingers/factory.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 from pinger import Pinger
 from pingers.unix import UnixPinger
 from pingers.windows import WindowsPinger
@@ -6,14 +7,17 @@ from pingers.windows import WindowsPinger
 
 class PingerFactory:
 
-    def get_system_pinger(self)-> Pinger:
+    def get_system_pinger(self) -> Pinger:
+        pinger = None
         if sys.platform.startswith('freebsd') or sys.platform.startswith('linux') or sys.platform.startswith('aix') \
                 or sys.platform.startswith('darwin'):
-            return UnixPinger()
+            pinger = UnixPinger()
         elif sys.platform.startswith('win32'):
-            return WindowsPinger()
+            pinger = WindowsPinger()
         elif sys.platform.startswith('cygwin'):
             raise NotImplementedError(self._get_exception_message('cygwin'))
+        logging.debug(f"Creating {pinger.__class__.__name__} for {sys.platform}")
+        return pinger
 
     @staticmethod
     def _get_exception_message(platform_uname) -> str:
