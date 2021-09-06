5243c4153e135aaf4b3f652f298824f360a4140f
Kristian Fischer <khfishy@gmail.com>
2021-09-06 11:33:06 -0700

    Added basic configuration for logging and replace print statements

--- pingermain.py
+++ pingermain.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import logging
 import time
 from typing import List
 from processors.multithread import MultiThreadPingProcessor
@@ -8,10 +9,16 @@ from processors.multithread import MultiThreadPingProcessor
 def read_hosts() -> List[str]:
     with open('data.json') as file_handle:
         json_data = json.load(file_handle)
+        logging.debug(f"Parsed {json_data}")
         try:
             hosts = json_data["hosts"]
         except KeyError as err:
-            raise Exception("The list of `hosts' could not be parsed from data.json")
+            logging.error("The list of 'hosts' could not be parsed from data.json")
+            raise Exception(err)
+        if len(hosts) < 1:
+            err_msg = "data.json did not contain any hosts to ping."
+            logging.error(err_msg)
+            raise Exception(err_msg)
         return hosts
 
 
@@ -19,8 +26,6 @@ def do_ping_job(args: argparse.Namespace):
     start_time = time.time()
     processor = MultiThreadPingProcessor(args.pool)
     hosts = read_hosts()
-    if len(hosts) < 1:
-        raise Exception("data.json did not contain any hosts to ping.")
     ping_results = processor.do_ping_job(hosts, args.iterations)
     max_ping = 0
     max_host = ""
@@ -29,8 +34,8 @@ def do_ping_job(args: argparse.Namespace):
         max_ping = max(max_ping, result.total_ping_time)
         if prev_ping != max_ping:
             max_host = result.host
-    print(f"Longest job took {max_ping} seconds to ping {max_host}")
-    print(f"Program took {time.time() - start_time} seconds to run")
+    logging.info(f"Longest job took {max_ping} seconds to ping {max_host}")
+    logging.info(f"Program took {time.time() - start_time} seconds to run")
 
 
 def check_positive(value):
@@ -45,8 +50,13 @@ def check_positive(value):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(relativeCreated)6d %(threadName)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
     parser = argparse.ArgumentParser(description='Ping some websites.')
     parser.add_argument('--pool', type=check_positive, help='Number of pools for executor', default=5)
     parser.add_argument('--iterations', type=check_positive, help='Number of times to ping each website', default=5)
-    do_ping_job(parser.parse_args())
+    parsed_args = parser.parse_args()
+    logging.info(f"Number of execution pools: {parsed_args.pool}")
+    logging.info(f"Number of ping iterations: {parsed_args.iterations}")
+    do_ping_job(parsed_args)
 
