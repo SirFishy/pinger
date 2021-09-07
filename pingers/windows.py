d13397dc417194514f35b4841a13538ea5077c67
Kristian Fischer <khfishy@gmail.com>
2021-09-07 16:48:24 -0700

    Removed duplciate log message

--- pingers/windows.py
+++ pingers/windows.py
@@ -8,7 +8,6 @@ class WindowsPinger(Pinger):
 
     @Pinger.log_ping_decorator
     def _subprocess_ping(self, host: str, iterations: int):
-        logging.info(f"Pinging {host} {iterations} times.")
         ping_process = subprocess.Popen(["ping", "-n", f"{iterations}", host], stdout=subprocess.PIPE,
                                         stderr=subprocess.PIPE, text=True)
         return ping_process.communicate()
